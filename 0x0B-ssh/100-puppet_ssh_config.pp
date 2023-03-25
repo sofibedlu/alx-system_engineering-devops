7d9a891c5db6de096c43d69a2314aaeed40d0ce6
sofi <bedlusofi@gmail.com>
2023-03-25 12:54:34 +0000

    update-doc

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
+#configure the ssh-client to use private_key ~/.ssh/school and refuse to authenticate using a password
 file { '/home/vagrant/.ssh/config':
 ensure  => 'present',
 content => "IdentityFile ~/.ssh/school\nPasswordAuthentication no\n",
