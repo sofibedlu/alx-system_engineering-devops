3ac77366b0059658e59c3d212625ba680af1b7c8
sofi <bedlusofi@gmail.com>
2023-03-25 12:50:44 +0000

    update-doc

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,4 @@
-class ssh_client {
-  file { '/home/vagrant/.ssh/config':
-    ensure  => 'present',
-    content => "IdentityFile ~/.ssh/school\nPasswordAuthentication no\n",
-  }
+file { '/home/vagrant/.ssh/config':
+ensure  => 'present',
+content => "IdentityFile ~/.ssh/school\nPasswordAuthentication no\n",
 }
-
-include ssh_client
