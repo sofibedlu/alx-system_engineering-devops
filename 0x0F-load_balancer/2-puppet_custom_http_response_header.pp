59d215aa0eb0c680b4d197e02d75199c90d8c814
sofi <bedlusofi@gmail.com>
2023-04-03 19:37:03 +0000

    update-config content

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,9 +6,8 @@ path    => '/usr/bin',
 }
 
 package { 'nginx':
-ensure          => installed,
-provider        => 'apt-get',
-install_options => '-y',
+ensure   => installed,
+provider => 'apt-get',
 }
 
 file { '/var/www/html/index.html':
@@ -19,26 +18,23 @@ file { '/var/www/html/404.html':
 content => 'Ceci n\'est pas une page\n',
 }
 
-$config = @(END)
- server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-        error_page 404 /404.html;
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
-	if ($request_filename ~ redirect_me){
-		rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-	}
-	location / {
-		 try_files $uri $uri/ =404;
-	}
- }
-END
-
 file { '/etc/nginx/sites-enabled/default':
 ensure  => 'file',
-content => $config,
+content =>
+  'server {
+	  listen 80 default_server;
+	  listen [::]:80 default_server;
+	  root /var/www/html;
+          error_page 404 /404.html;
+	  index index.html index.htm index.nginx-debian.html;
+	  server_name _;
+	  if (\$request_filename ~ redirect_me){
+		  rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+	  }
+	  location / {
+	  	  try_files \$uri \$uri/ =404;
+	  }
+  }',
 }
 
 new_string="http {\n\tadd_header X-Served-By \$hostname;"
