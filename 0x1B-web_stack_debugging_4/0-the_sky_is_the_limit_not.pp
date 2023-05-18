f638616e02b26e5037617640b313b5232a157b1a
sofi <bedlusofi@gmail.com>
2023-05-18 15:42:00 +0000

    add-comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
-exec { 'increase ulimit for nginx':
+# this manifest increase u limit for nginx
+exec { 'set ulimit for nginx':
   command => '/bin/sed -i "s/ULIMIT.*/ULIMIT=\"-n 5000\"/" /etc/default/nginx'
 } -> exec { 'restart nginx':
   command => '/usr/sbin/service nginx restart',
